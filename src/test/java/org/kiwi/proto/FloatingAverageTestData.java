91f0bebabc329ea8c53caae11c0a83403de2ee37
kiwilisk <kiwilisk@gmail.com>
2017-03-19 13:11:52 +0100

    Enable FloatingAverageRepository to store and load multiple averages
    * Introduce asynchronous retrieval and storage of FloatingAverages

--- src/test/java/org/kiwi/proto/FloatingAverageTestData.java
+++ src/test/java/org/kiwi/proto/FloatingAverageTestData.java
@@ -5,7 +5,6 @@ import static java.util.Arrays.asList;
 import static org.kiwi.proto.FloatingAverageProtos.FloatingAverage.AlertState.NONE;
 
 import java.time.LocalDate;
-import java.util.Collection;
 import org.kiwi.proto.FloatingAverageProtos.FloatingAverage;
 import org.kiwi.proto.FloatingAverageProtos.Quote;
 
@@ -14,19 +13,7 @@ public class FloatingAverageTestData {
     private static final long DATE_AS_EPOCH_SECOND = LocalDate.of(2017, 1, 23).atStartOfDay().toInstant(UTC)
             .getEpochSecond();
 
-    public static FloatingAverage createFloatingAverage() {
-        return FloatingAverage.newBuilder()
-                .setId("bitcoin")
-                .setName("Bitcoin")
-                .setSymbol("BTC")
-                .setAlertState(NONE)
-                .setClosingDate(DATE_AS_EPOCH_SECOND)
-                .setCurrentAverage("1229.68")
-                .addAllQuotes(createQuotes())
-                .build();
-    }
-
-    private static Collection<Quote> createQuotes() {
+    public static FloatingAverage createBitcoinTestData() {
         Quote quote1 = Quote.newBuilder()
                 .setAverage("1199.67")
                 .setValue("1201.75")
@@ -37,6 +24,31 @@ public class FloatingAverageTestData {
                 .setValue("1210.02")
                 .setUpdatedAt(DATE_AS_EPOCH_SECOND)
                 .build();
-        return asList(quote1, quote2);
+        return FloatingAverage.newBuilder()
+                .setId("bitcoin")
+                .setName("Bitcoin")
+                .setSymbol("BTC")
+                .setAlertState(NONE)
+                .setClosingDate(DATE_AS_EPOCH_SECOND)
+                .setCurrentAverage("1229.68")
+                .addAllQuotes(asList(quote1, quote2))
+                .build();
+    }
+
+    public static FloatingAverage createEthereumTestData() {
+        Quote etherumQuote = Quote.newBuilder()
+                .setAverage("18.2812")
+                .setValue("18.2812")
+                .setUpdatedAt(1485129600)
+                .build();
+        return FloatingAverage.newBuilder()
+                .setId("ethereum")
+                .setName("Ethereum")
+                .setSymbol("ETH")
+                .setAlertState(NONE)
+                .setClosingDate(1485129600)
+                .setCurrentAverage("18.2812")
+                .addQuotes(etherumQuote)
+                .build();
     }
 }
